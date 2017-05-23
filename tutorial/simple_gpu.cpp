421203661a724fa22dd8eaebfa8be5826b530c8d
ankdesh <ankdesh>
2017-05-23 16:35:04 +0530

    Fix simple_gpu example to gen opencl code

--- tutorial/simple_gpu.cpp
+++ tutorial/simple_gpu.cpp
@@ -1,10 +1,11 @@
 #include <Halide.h>
 #include <stdio.h>
+#include <dlfcn.h>
 
 int main(int argc, char **argv) {
     // Define a gradient function.
     Halide::Func f;
-    Halide::Var x, y;
+    Halide::Var x("x"), y("y");
     f(x, y) = x + y;
 
     // Schedule f on the GPU in 16x16 tiles.
@@ -14,7 +15,7 @@ int main(int argc, char **argv) {
     Halide::Target target = Halide::get_host_target();
 
     // Enable OpenCL as the GPU backend.
-    //target.set_feature(Halide::Target::OpenCL);
+    target.set_feature(Halide::Target::OpenCL);
 
     // Enable debugging so that you can see what OpenCL API calls we do.
     target.set_feature(Halide::Target::Debug);
@@ -24,8 +25,9 @@ int main(int argc, char **argv) {
 
     // Run it.
     //Halide::Buffer<int> result = f.realize(32, 32);
-    f.compile_to_c("simple_gpu.c", {});
     f.print_loop_nest();
+    //dlopen("libOpenCL.so", RTLD_LAZY);
+    f.compile_to_lowered_stmt("simple_gpu.txt", {}, Halide::StmtOutputFormat::Text, target);
     
 	
     // Print the result.
