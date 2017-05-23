6d0e680507a451bef0bd8e5d54b7c1737fd2b355
ankdesh <ankdesh>
2017-05-23 19:04:55 +0530

    Add code to simple_gpu for equivalent CPU code gen for comparision

--- tutorial/simple_gpu.cpp
+++ tutorial/simple_gpu.cpp
@@ -5,11 +5,12 @@
 int main(int argc, char **argv) {
     // Define a gradient function.
     Halide::Func f;
-    Halide::Var x("x"), y("y");
+    Halide::Var x("x"), y("y"), xi("xi"), yi("yi");
     f(x, y) = x + y;
 
     // Schedule f on the GPU in 16x16 tiles.
-    f.gpu_tile(x, y, 16, 16);
+    //f.tile(x, y, xi, yi, 16, 16);
+    //f.gpu_tile(x, y, 16, 16);
 
     // Construct a target that uses the GPU.
     Halide::Target target = Halide::get_host_target();
@@ -27,7 +28,7 @@ int main(int argc, char **argv) {
     //Halide::Buffer<int> result = f.realize(32, 32);
     f.print_loop_nest();
     //dlopen("libOpenCL.so", RTLD_LAZY);
-    f.compile_to_lowered_stmt("simple_gpu.txt", {}, Halide::StmtOutputFormat::Text, target);
+    f.compile_to_lowered_stmt("simple_gpu.html", {}, Halide::StmtOutputFormat::HTML, target);
     
 	
     // Print the result.
